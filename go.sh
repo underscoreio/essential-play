c01e346a9ab71343d67edd147968ce7135fc8f36
Dave Gurnell <dave@untyped.com>
2014-11-14 09:06:32 +0000

    Added #! header to go.sh

--- go.sh
+++ go.sh
@@ -1,40 +1,40 @@
-#
-#          --epub-stylesheet ./essential-play/css/print.css \
-#--template=src/layouts/page.html \
+#!/usr/bin/env bash
 
-pandoc -S                               \
-          -o essential-play.pdf          \
-          --table-of-contents           \
-          --toc-depth=4                 \
-          --from=markdown+multiline_tables+fenced_code_blocks+fenced_code_attributes \
-          --highlight-style tango     \
-pandoc/title.txt                      \
-src/pages/index.md                    \
-src/pages/basics/index.md             \
-src/pages/basics/actions.md           \
-src/pages/basics/routes.md            \
-src/pages/basics/requests.md          \
-src/pages/basics/results.md           \
-src/pages/basics/failure.md           \
-src/pages/html/index.md               \
-src/pages/html/templates.md           \
-src/pages/html/forms.md               \
-src/pages/html/form-templates.md      \
-src/pages/json/index.md               \
-src/pages/json/values.md              \
-src/pages/json/writes.md              \
-src/pages/json/reads.md               \
-src/pages/json/formats.md             \
-src/pages/json/custom1.md             \
-src/pages/json/custom2.md             \
-src/pages/json/custom3.md             \
-src/pages/json/failure.md             \
-src/pages/async/index.md              \
-src/pages/async/futures.md            \
-src/pages/async/executioncontexts.md  \
-src/pages/async/actions.md            \
-src/pages/async/ws.md                 \
-src/pages/async/failure.md            \
-src/pages/links.md                    \
+# --epub-stylesheet ./essential-play/css/print.css                           \
+# --template=src/layouts/page.html                                           \
+# --toc-depth=4                                                              \
 
+pandoc -S                                                                    \
+  -o essential-play.pdf                                                      \
+  --table-of-contents                                                        \
+  --from=markdown+multiline_tables+fenced_code_blocks+fenced_code_attributes \
+  --highlight-style tango                                                    \
+  pandoc/title.txt                                                           \
+  src/pages/index.md                                                         \
+  src/pages/basics/index.md                                                  \
+  src/pages/basics/actions.md                                                \
+  src/pages/basics/routes.md                                                 \
+  src/pages/basics/requests.md                                               \
+  src/pages/basics/results.md                                                \
+  src/pages/basics/failure.md                                                \
+  # src/pages/html/index.md                                                    \
+  # src/pages/html/templates.md                                                \
+  # src/pages/html/forms.md                                                    \
+  # src/pages/html/form-templates.md                                           \
+  # src/pages/json/index.md                                                    \
+  # src/pages/json/values.md                                                   \
+  # src/pages/json/writes.md                                                   \
+  # src/pages/json/reads.md                                                    \
+  # src/pages/json/formats.md                                                  \
+  # src/pages/json/custom1.md                                                  \
+  # src/pages/json/custom2.md                                                  \
+  # src/pages/json/custom3.md                                                  \
+  # src/pages/json/failure.md                                                  \
+  # src/pages/async/index.md                                                   \
+  # src/pages/async/futures.md                                                 \
+  # src/pages/async/executioncontexts.md                                       \
+  # src/pages/async/actions.md                                                 \
+  # src/pages/async/ws.md                                                      \
+  # src/pages/async/failure.md                                                 \
+  # src/pages/links.md                                                         \
 
