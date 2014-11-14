33aa332ba4ae1fbe030b014db15817af80916486
Jonathan Ferguson <jono@spiralarm.com>
2014-11-14 12:24:30 +1100

    script to create PDFs

---
+++ go.sh
@@ -0,0 +1,36 @@
+pandoc -S                               \
+          -o essential-play.pdf         \
+          --table-of-contents           \
+          --toc-depth=4                 \
+          --from=markdown+multiline_tables+fenced_code_blocks+fenced_code_attributes                 \
+          --epub-stylesheet ./essential-play/css/print.css \
+          --highlight-style tango     \
+pandoc/title.txt                      \
+src/pages/index.md                    \
+src/pages/basics/index.md             \
+src/pages/basics/actions.md           \
+src/pages/basics/routes.md            \
+src/pages/basics/requests.md          \
+src/pages/basics/results.md           \
+src/pages/basics/failure.md           \
+src/pages/html/index.md               \
+src/pages/html/templates.md           \
+src/pages/html/forms.md               \
+src/pages/html/form-templates.md      \
+src/pages/json/index.md               \
+src/pages/json/values.md              \
+src/pages/json/writes.md              \
+src/pages/json/reads.md               \
+src/pages/json/formats.md             \
+src/pages/json/custom1.md             \
+src/pages/json/custom2.md             \
+src/pages/json/custom3.md             \
+src/pages/json/failure.md             \
+src/pages/async/index.md              \
+src/pages/async/futures.md            \
+src/pages/async/executioncontexts.md  \
+src/pages/async/actions.md            \
+src/pages/async/ws.md                 \
+src/pages/async/failure.md            \
+
+
